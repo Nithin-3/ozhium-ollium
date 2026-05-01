4bbccfddb3009a6cbb09911a31e076709cce9b34
ninja nithin <nithin3dev@gmail.com>
2026-05-01 21:37:35 +0530

    fix:accept only desided value

--- src/ui/config.c
+++ src/ui/config.c
@@ -1,4 +1,5 @@
 #include "ui/config.h"
+#include "glib.h"
 #include "glibconfig.h"
 #include "tool.h"
 
@@ -33,31 +34,59 @@ void loadConfig(void) {
 		return;
 	}
 
-	if (g_key_file_has_key(keyfile, "osd", "vertical-align", NULL))
-		appConfig.verticalAlign = g_key_file_get_string(keyfile, "osd", "vertical-align", NULL);
-	if (g_key_file_has_key(keyfile, "osd", "horizontal-align", NULL))
-		appConfig.horizontalAlign = g_key_file_get_string(keyfile, "osd", "horizontal-align", NULL);
-	if (g_key_file_has_key(keyfile, "osd", "orientation", NULL))
-		appConfig.orientation = g_key_file_get_string(keyfile, "osd", "orientation", NULL);
-	if (g_key_file_has_key(keyfile, "osd", "timeout", NULL))
-		appConfig.timeOut = g_key_file_get_integer(keyfile, "osd", "timeout", NULL);
-	if (g_key_file_has_key(keyfile, "osd", "x", NULL))
-		appConfig.x = g_key_file_get_integer(keyfile, "osd", "x", NULL);
-	if (g_key_file_has_key(keyfile, "osd", "y", NULL))
-		appConfig.y = g_key_file_get_integer(keyfile, "osd", "y", NULL);
-	if (g_key_file_has_key(keyfile, "osd", "margin", NULL))
-		appConfig.margin = g_key_file_get_integer(keyfile, "osd", "margin", NULL);
-
-	if (g_key_file_has_key(keyfile, "slider", "invert_dir", NULL))
-		icons.backlight = g_key_file_get_string(keyfile, "slider", "invert_dir", NULL);
-
+	if (g_key_file_has_key(keyfile, "osd", "vertical-align", NULL)) {
+		gchar *val = g_key_file_get_string(keyfile, "osd", "vertical-align", &error);
+		if (val && (strcmp(val, "top") == 0 || strcmp(val, "center") == 0 || strcmp(val, "bottom") == 0))
+			appConfig.verticalAlign = val;
+		else
+			g_free(val);
+	}
+	if (g_key_file_has_key(keyfile, "osd", "horizontal-align", NULL)) {
+		gchar *val = g_key_file_get_string(keyfile, "osd", "horizontal-align", &error);
+		if (val && (strcmp(val, "left") == 0 || strcmp(val, "right") == 0 || strcmp(val, "center") == 0))
+			appConfig.horizontalAlign = val;
+		else
+			g_free(val);
+	}
+	if (g_key_file_has_key(keyfile, "osd", "orientation", NULL)) {
+		gchar *val = g_key_file_get_string(keyfile, "osd", "orientation", &error);
+		if (val && (strcmp(val, "vertical") == 0 || strcmp(val, "horizontal") == 0))
+			appConfig.orientation = val;
+		else
+			g_free(val);
+	}
+	if (g_key_file_has_key(keyfile, "osd", "timeout", NULL)) {
+		gint val = g_key_file_get_integer(keyfile, "osd", "timeout", &error);
+		if (!error && val > 0)
+			appConfig.timeOut = val;
+	}
+	if (g_key_file_has_key(keyfile, "osd", "x", NULL)) {
+		gint val = g_key_file_get_integer(keyfile, "osd", "x", &error);
+		if (!error && val >= 0)
+			appConfig.x = val;
+	}
+	if (g_key_file_has_key(keyfile, "osd", "y", NULL)) {
+		gint val = g_key_file_get_integer(keyfile, "osd", "y", &error);
+		if (!error && val >= 0)
+			appConfig.y = val;
+	}
+	if (g_key_file_has_key(keyfile, "osd", "margin", NULL)) {
+		gint val = g_key_file_get_integer(keyfile, "osd", "margin", &error);
+		if (!error && val >= 0)
+			appConfig.margin = val;
+	}
+	if (g_key_file_has_key(keyfile, "slider", "invert_dir", NULL)) {
+		gboolean val = g_key_file_get_boolean(keyfile, "slider", "invert_dir", &error);
+		if (!error)
+			sliderConfig.invert_dir = val;
+	}
 
 	if (g_key_file_has_key(keyfile, "icons", "backlight", NULL))
-		icons.backlight = g_key_file_get_string(keyfile, "icons", "backlight", NULL);
+		icons.backlight = g_key_file_get_string(keyfile, "icons", "backlight", &error);
 	if (g_key_file_has_key(keyfile, "icons", "volume", NULL))
-		icons.volume = g_key_file_get_string(keyfile, "icons", "volume", NULL);
+		icons.volume = g_key_file_get_string(keyfile, "icons", "volume", &error);
 	if (g_key_file_has_key(keyfile, "icons", "volume-mute", NULL))
-		icons.volumeMute = g_key_file_get_string(keyfile, "icons", "volume-mute", NULL);
+		icons.volumeMute = g_key_file_get_string(keyfile, "icons", "volume-mute", &error);
 
 	g_key_file_unref(keyfile);
 }
