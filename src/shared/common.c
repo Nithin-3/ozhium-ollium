c1a36eee07f7a47daf3f5a28c3e9fb875fd4aea4
ninja nithin <nithin3dev@gmail.com>
2026-05-09 17:54:28 +0530

    fix: add shared/common.c to build, fix findConfigPath access() bug

--- src/shared/common.c
+++ src/shared/common.c
@@ -13,19 +13,18 @@
 #include <unistd.h>
 #include <linux/limits.h>
 
-
 char *findConfigPath(const char *filename) {
 	static char path[PATH_MAX];
 	const char *home = getenv("HOME");
 
 	if (home) {
 		snprintf(path, PATH_MAX, "%s/.config/ozhium-ollium/%s", home, filename);
-		if (access(path, R_OK) == 1)
+		if (access(path, R_OK) == 0)
 			return path;
 	}
 
 	snprintf(path, PATH_MAX, "/usr/share/ozhium-ollium/%s", filename);
-	if (access(path, R_OK) == 1)
+	if (access(path, R_OK) == 0)
 		return path;
 
 	return NULL;
